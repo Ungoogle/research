3d969bc0fa04d943dbfdbf902ee1db16d63ce546
Rodney Lightfoot <10424075+Ungoogle@users.noreply.github.com>
2020-12-18 22:58:18 -0600

    Create Search-Logs-App.js
    Was my system involved also? This is my 1st idea about sunburst hack affects knowledge assurance. Excuse my code.

---
+++ Search-Logs-App.js
@@ -0,0 +1,10 @@
+# Let's see if we were affected by Sunburst.
+# https://github.com/Ungoogle
+#
+VAR data = GET(URLs.TxT);
+f(Find);
+eval(*|data());
+return(Find());
+Alert("See root folder for results page.");
+document.write(.\Result-of-Log-SCAN.html);
+End;
